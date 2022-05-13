f01984df0f3333225160b3980fd45e6fdfd48861
unknown <rkdxogh1987@gmail.com>
2022-05-13 18:23:03 +0900

    2022/05/13 Ch09 실습

---
+++ Ch09/src/main/resources/static/js/student.js
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+$(function(){
+	$('.student_List').click(function(){})
+	$('.student_Register').click(function(){})
+	$('.student_Modify').click(function(){})
+	$('.student_Delete').click(function(){})
+})
